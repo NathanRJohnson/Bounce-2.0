494ebf5d63b37697ae976603ccbbe46eb00f089d
Nathan Johnson <robert.minecrafter.nathan@gmail.com>
2018-10-10 12:49:23 -0400

    Getting Started

---
+++ desktop/src/cs/bounce/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package cs.bounce.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import cs.bounce.GamMenu;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GamMenu(), config);
+	}
+}
