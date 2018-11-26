5441719a46382a96ed52ab08054a4bcfb1e3b304
Noah Hatt <hattnoah@gmail.com>
2018-11-26 13:09:13 -0500

    I have an idea

--- core/src/cs/bounce/Screens/ScrStart.java
+++ core/src/cs/bounce/Screens/ScrStart.java
@@ -121,6 +121,7 @@ public class ScrStart implements Screen, InputProcessor {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+
         return false;
     }
 
