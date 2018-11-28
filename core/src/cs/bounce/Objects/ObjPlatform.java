12945a3ac39dd34413bb03aa7f10cbf47e2b0af9
Nathan Johnson <robert.minecrafter.nathan@gmail.com>
2018-11-28 12:51:38 -0500

    Revert "Integrated Multiside Platforms"
    This reverts commit 254d603801e31a4488b2f5c51189638933053e52.

--- core/src/cs/bounce/Objects/ObjPlatform.java
+++ core/src/cs/bounce/Objects/ObjPlatform.java
@@ -1,85 +1,26 @@
 package cs.bounce.Objects;
 
-
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Intersector;
-
+import com.badlogic.gdx.math.Polygon;
+import com.badlogic.gdx.math.Vector2;
 
 public class ObjPlatform extends SprObstacle {
-    private static boolean isTopHitting, isBotHitting, isLeftHitting, isRightHitting;
 
     public ObjPlatform(String sFile, float fX, float fY, float fW, float fH) {
         super(sFile, fX, fY, fW, fH);
 
     }
 
-
-    public void isHit(SprHero sHero) {
-        isTopHitting = Intersector.intersectSegmentPolygon(getTopRight(), getTopLeft(), sHero.getPolygon());
-        isBotHitting = Intersector.intersectSegmentPolygon(getBotRight(), getBotLeft(), sHero.getPolygon());
-        isLeftHitting = Intersector.intersectSegmentPolygon(getTopLeft(), getBotLeft(), sHero.getPolygon());
-        isRightHitting = Intersector.intersectSegmentPolygon(getTopRight(), getBotRight(), sHero.getPolygon());
-
+    public void isHit(Polygon pHero, SprHero sHero) {
         if (!sHero.getJumpState()) {
-            if (isTopHitting) {
-                if (fY != 0) {
-                    sHero.setPos(sHero.getPos().x, fY + getHeight() - 15);
-                    sHero.setMaxHeight();
-                } else {
-                    sHero.setPos(sHero.getPos().x, sHero.getHeight() - 15);
-
-                }
-                System.out.println("Hit Top");
-                sHero.setVel(sHero.getVel().x, 0);
+            if (Intersector.overlapConvexPolygons(pHero, plyObstacle)) {
                 sHero.setCanJump(true);
-            }
-        }
-        if (isBotHitting) {
-            sHero.setCanJump(false);
-            System.out.println("Hit Bottom");
-            if (sHero.getVel().y > 0) {
-                sHero.setVel(sHero.getVel().x, sHero.getVel().y * -1);
-                sHero.setVel(sHero.getVel().x, sHero.getVel().y / 2);
-            }
-        }
+                sHero.setPos(sHero.getPos().x, plyObstacle.getY() + sHero.getHeight() - 15);
+                sHero.setVel(sHero.getVel().x, 0);
 
-        if (isRightHitting || isLeftHitting) {
-            if (!isTopHitting) {
-                sHero.setVel(0, sHero.getVel().y);
-                if (isRightHitting) {
-                    System.out.println("Hit Left");
-                    sHero.setPos(sHero.getX() + 1, sHero.getPos().y);
-                }
-                if (isLeftHitting) {
-                    System.out.println("Hit Right");
-                    sHero.setPos(sHero.getX() - 1, sHero.getPos().y);
-                }
             }
         }
     }
-
-    public boolean checkHit(SprHero sHero) {
-        //isHitting = Intersector.overlapConvexPolygons(sHero.getPolygon(), plyObstacle);
-        isTopHitting = Intersector.intersectSegmentPolygon(getTopRight(), getTopLeft(), sHero.getPolygon());
-        isBotHitting = Intersector.intersectSegmentPolygon(getBotRight(), getBotLeft(), sHero.getPolygon());
-        isLeftHitting = Intersector.intersectSegmentPolygon(getTopLeft(), getBotLeft(), sHero.getPolygon());
-        isRightHitting = Intersector.intersectSegmentPolygon(getTopRight(), getBotRight(), sHero.getPolygon());
-        if (isTopHitting) {
-            // System.out.println("Hit Top");
-            return true;
-        }
-        if (isBotHitting) {
-            //  System.out.println("Hit Bot");
-            return true;
-        }
-        if (isLeftHitting) {
-
-            return true;
-        }
-        if (isRightHitting) {
-
-            return true;
-        }
-        return false;
-    }
-
 }
