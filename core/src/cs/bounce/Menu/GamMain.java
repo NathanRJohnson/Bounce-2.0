105d5d67c46313fa5e395701c32434652382b330
Noah Hatt <hattnoah@gmail.com>
2019-01-08 12:37:10 -0500

    Fixed this aswell

--- core/src/cs/bounce/Menu/GamMain.java
+++ core/src/cs/bounce/Menu/GamMain.java
@@ -8,28 +8,19 @@ import cs.bounce.Screens.*;
 
 public class GamMain extends Game {
 
-	ScrLevels scrLevels;
-	ScrLvl1 scrLvl1;
-	ScrLvl2 scrLvl2;
-	ScrStart scrStart;
+    ScrLvl1 scrLvl1;
 
-	int nScreen;
-	boolean onMenu = false;
+    int nScreen;
+    boolean onMenu = false;
 
-	public void updateScreen(int _nScreen) {
-		nScreen = _nScreen;
-		if (nScreen == 0) {
-			setScreen(scrStart);
-		} else if (nScreen == 1) {
-			setScreen(scrLevels);
-		} else if (nScreen == 2) {
-			setScreen(scrLvl1);
-		} else if (nScreen == 3) {
-			setScreen(scrLvl2);
-		}
+    public void updateScreen(int _nScreen) {
+        nScreen = _nScreen;
+        if (nScreen == 2) {
+            setScreen(scrLvl1);
+        }
+    }
 
-	}
-	/*
+    /*
         public boolean Menu() {
             if (Gdx.input.isKeyPressed(Input.Keys.M)) {
                 System.out.println("MENU");
@@ -38,42 +29,39 @@ public class GamMain extends Game {
             return false;
         }
     */
-		@Override
-		public void create () {
-			scrStart = new ScrStart(this);
-			scrLevels = new ScrLevels(this);
-			scrLvl1 = new ScrLvl1(this);
-			scrLvl2 = new ScrLvl2(this);
-			//scrLvl3 =new ScrLvl3(this);
-			updateScreen(0);
-		}
+    @Override
+    public void create() {
+        scrLvl1 = new ScrLvl1(this);
+        //scrLvl3 =new ScrLvl3(this);
+        updateScreen(2);
+    }
 
-		@Override
-		public void render () {
-			super.render();
-			//Menu();
-		}
+    @Override
+    public void render() {
+        super.render();
+        //Menu();
+    }
 
-		@Override
+    @Override
 
-		public void pause () {
+    public void pause() {
 
-		}
+    }
 
-		@Override
-		public void resume () {
+    @Override
+    public void resume() {
 
-		}
+    }
 
-		@Override
-		public void resize ( int width, int height){
+    @Override
+    public void resize(int width, int height) {
 
-		}
+    }
 
-		@Override
-		public void dispose () {
-			super.dispose();
-		}
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
 
 
 }
